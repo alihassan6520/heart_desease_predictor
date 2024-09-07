3d7b63425d0ffd8e276867eea43609d19fd02345
Ali Hassan <ali460010@gmail.com>
2024-09-08 03:56:33 +0500

    Cleaned code

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-plt.style.use('bmh')
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
