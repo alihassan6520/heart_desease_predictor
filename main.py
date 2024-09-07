c323983ba6b834f6e89f21a95092584b860d64a1
Ali Hassan <ali460010@gmail.com>
2024-09-08 03:09:18 +0500

    Added requirements

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# plt.style.use('bmh')
-# from sklearn.model_selection import train_test_split, GridSearchCV
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.metrics import accuracy_score, classification_report
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.svm import SVC
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.metrics import confusion_matrix
-# from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('bmh')
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
 import joblib
 import streamlit as st
 
